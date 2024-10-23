59303dca2dff29d9c2f152d10ad13fa38fe3ff8a
Maxime Naulleau <maxime.naulleau@beta.gouv.fr>
2024-10-23 11:17:12 +0200

    Simplify import of nomination files tests

--- apps/api/src/data-administrator-context/business-logic/use-cases/nomination-files-import/import-nomination-files.use-case.spec.ts
+++ apps/api/src/data-administrator-context/business-logic/use-cases/nomination-files-import/import-nomination-files.use-case.spec.ts
@@ -16,12 +16,15 @@ const firstHeader = `								Eléments du dossier								Règles automatisées d
 const secondHeader = `Point(s) d'attention(s) repéré(s) dans le dossier	N° dossier                                                   	Magistrat	Formation	Date d'échéance	Etat	Transparence	Rapporteur(s)	Grade actuel	Poste actuel	Poste pressenti	Rang	Date de naissance	Historique	Observants		Mutation en - de 3 ans	Passer au 1er grade	Passe au grade "HH"	Prendre son grade sur place	Poste "profilé"	Nomination à la CC	"Outremer sur Outremer"	Siège <> Parquet et TJ <> CA	Siège <> Parquet du même ressort	Siège <> Parquet d'une même juridiction	Prendre son grade sur place après 7 ans	Ministère de la Justice à - de 3 ans d'exercice		Cabinet du ministre	Inscription au tableau pour prise de grade	Accéder à la HH sans avoir fait 2 postes au 1er grade	Prof. jur. dans le ressort du TJ il y a - de 5 ans	Conflit d'intérêt avec parcours pré magistrature	Conflit d'intérêt avec la prof. d'un proche	Evaluations	Eléments disciplinaires	Conditions de nomination HH 		Point d'attention sur ce dossier ?	Grade actuel	Intitulé du poste actuel	Reformulation du poste actuel	Date de prise du poste actuel	Lieu d'exercice du poste actuel (nom de la juridiction)	Localisation du poste actuel (Métropole ou Outremer)	Cour d'appel de rattachement du poste actuel	Poste au Ministère ?	Grade du poste pressenti	Titre du poste pressenti	Poste profilé ?	Intitulé du poste pressenti	Reformulation du poste pressenti, sans le grade II	Reformulation du poste pressenti, sans les grades II et I	Reformulation du poste pressenti, sans les grades II, I et HH	Lieu d'exercice du poste pressenti (nom de la juridiction)	Localisation du poste pressenti (Métropole ou Outremer)	Cour d'appel de rattachement du poste pressenti	Date pour la prise de poste (si nomination confirmée)	Observations`;
 const allRulesValidatedLine = `TRUE	1 (parq.)	Marcel Dupont Ep. François 	Siège	10/11/2024	Nouveau	Automne 2024	LUC Loïc 	I	Avocat général - service extraordinaire CC  PARIS 	Premier avocat général CC  PARIS - HH 	(2 sur une liste de 2)	1/11/1961	- blablablablabla 	  JEAN PASCAL VPI TJ PARIS (9 sur une liste de 11)		TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE		TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE		TRUE	I	Avocat général - service extraordinaire CC  PARIS	Avocat	mars 2022	PARIS	Métropole	CA PARIS	FALSE	HH	Premier	FALSE	Premier avocat général CC  PARIS - HH	Premier avocat général I  PARIS - HH	Premier avocat général CC  PARIS - I	Premier avocat général CC  PARIS	PARIS	Métropole	CA PARIS	septembre 2024	  MATHIAS PASCAL VPI TJ PARIS (9 sur une liste de 11)`;
 const optionalFieldsAndOneRuleNotPrevalidated = `TRUE	3 (parq.)	Lucien Pierre 	Parquet		Avis restitué	Automne 2024	VICTOIRE Christian 	HH	Procureur de la République adjoint TJ  NIMES 	Avocat général CC  PARIS - HH 	2 sur une liste de 11)	22/8/1962	- blablablablabla 	   		TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE		FALSE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE	TRUE		TRUE	HH	Procureur de la République adjoint TJ  NIMES	Procureur	septembre 2012	NIMES	Métropole	CA NIMES	FALSE	HH	Avocat	FALSE	Avocat général CC  PARIS - HH	Avocat général CC  PARIS - HH	Avocat général CC  PARIS - HH	Avocat général CC  PARIS	PARIS	Métropole	CA PARIS	septembre 2024`;
+
 const fileToImportWithAllRulesPreValidated = `${firstHeader}
 ${secondHeader}
 ${allRulesValidatedLine}`;
+
 const fileToImportWithOptionalsOneRuleNotPrevalidated = `${firstHeader}
 ${secondHeader}
 ${optionalFieldsAndOneRuleNotPrevalidated}`;
+
 const fileToImportWithMultipleLines = `${firstHeader}
 ${secondHeader}
 ${allRulesValidatedLine}
@@ -58,102 +61,68 @@ describe('Import Nomination Files Use Case', () => {
   ])(
     'rejects a file with less than 3 lines',
     async ({ fileToImport, expectedError }) => {
-      expect(
-        new ImportNominationFilesUseCase(
-          nominationFileRepository,
-          dateTimeProvider,
-          uuidGenerator,
-          transactionPerformer,
-          domainEventRepository,
-        ).execute(fileToImport),
-      ).rejects.toThrow(expectedError);
+      expect(importAFile(fileToImport)).rejects.toThrow(expectedError);
     },
   );
 
-  it('parses a line with all values filled and all rules pre-validated at true', async () => {
-    const [nominationFileUuid] = uuidGenerator.genUuids(1);
-
-    await new ImportNominationFilesUseCase(
-      nominationFileRepository,
-      dateTimeProvider,
-      uuidGenerator,
-      transactionPerformer,
-      domainEventRepository,
-    ).execute(fileToImportWithAllRulesPreValidated);
-
-    expect(nominationFileRepository.nominationFiles).toEqual<
-      FakeNominationFileRepository['nominationFiles']
-    >({
-      1: getMarcelDupontModel(nominationFileUuid!, 1),
-    });
-    expect(domainEventRepository.events).toEqual<
-      NominationFilesImportedEvent[]
-    >([
+  it('informs about a new file imported', async () => {
+    await importAFile(fileToImportWithAllRulesPreValidated);
+    expectEvent(
       new NominationFilesImportedEvent(
         nominationFilesImportedEventId,
         { 1: getMarcelDupontRead(1) },
         dateTimeProvider.currentDate,
       ),
-    ]);
+    );
+  });
+
+  it('parses a line with all values filled and all rules pre-validated at true', async () => {
+    const [nominationFileUuid] = uuidGenerator.genUuids(1);
+    await importAFile(fileToImportWithAllRulesPreValidated);
+    expectNominationFiles(getMarcelDupontModel(nominationFileUuid!, 1));
   });
 
   it('parses a line with possible empty values unfilled and one rule pre-validated at true', async () => {
     const [nominationFileUuid] = uuidGenerator.genUuids(1);
-
-    await new ImportNominationFilesUseCase(
-      nominationFileRepository,
-      dateTimeProvider,
-      uuidGenerator,
-      transactionPerformer,
-      domainEventRepository,
-    ).execute(fileToImportWithOptionalsOneRuleNotPrevalidated);
-
-    expect(nominationFileRepository.nominationFiles).toEqual<
-      FakeNominationFileRepository['nominationFiles']
-    >({
-      1: getLucienPierreModel(nominationFileUuid!, 1),
-    });
-    expect(domainEventRepository.events).toEqual<
-      NominationFilesImportedEvent[]
-    >([
-      new NominationFilesImportedEvent(
-        nominationFilesImportedEventId,
-        { 1: getLucienPierreRead(1) },
-        dateTimeProvider.currentDate,
-      ),
-    ]);
+    await importAFile(fileToImportWithOptionalsOneRuleNotPrevalidated);
+    expectNominationFiles(getLucienPierreModel(nominationFileUuid!, 1));
   });
 
   it('saves two lines', async () => {
     const [firstUuid, secondUuid] = uuidGenerator.genUuids(2);
+    await importAFile(fileToImportWithMultipleLines);
+    expectNominationFiles(
+      getMarcelDupontModel(firstUuid!, 1),
+      getLucienPierreModel(secondUuid!, 2),
+    );
+  });
 
-    await new ImportNominationFilesUseCase(
+  const importAFile = async (fileToImport: string) =>
+    new ImportNominationFilesUseCase(
       nominationFileRepository,
       dateTimeProvider,
       uuidGenerator,
       transactionPerformer,
       domainEventRepository,
-    ).execute(fileToImportWithMultipleLines);
+    ).execute(fileToImport);
 
+  const expectEvent = async (...events: NominationFilesImportedEvent[]) =>
+    expect(domainEventRepository.events).toEqual(events);
+
+  const expectNominationFiles = async (
+    ...nominationFiles: NominationFileModel[]
+  ) =>
     expect(nominationFileRepository.nominationFiles).toEqual<
       FakeNominationFileRepository['nominationFiles']
-    >({
-      1: getMarcelDupontModel(firstUuid!, 1),
-      2: getLucienPierreModel(secondUuid!, 2),
-    });
-    expect(domainEventRepository.events).toEqual<
-      NominationFilesImportedEvent[]
-    >([
-      new NominationFilesImportedEvent(
-        nominationFilesImportedEventId,
-        {
-          [getMarcelDupontRead(1).rowNumber]: getMarcelDupontRead(1),
-          [getLucienPierreRead(2).rowNumber]: getLucienPierreRead(2),
-        },
-        dateTimeProvider.currentDate,
+    >(
+      nominationFiles.reduce(
+        (acc, nominationFile) => ({
+          ...acc,
+          [nominationFile.toSnapshot().rowNumber]: nominationFile,
+        }),
+        {},
       ),
-    ]);
-  });
+    );
 
   const getMarcelDupontModel = (
     uuid: string,
