8e023eaea0cd5dffbc46cf91f495a310c4c6944c
Ebrard Alexis <dakie62@MacBook-Pro-de-Ebrard.local>
2025-05-06 11:15:02 +0200

    Ajout de react Hook form

--- apps/webapp/src/secretariat-general/adapters/primary/components/SgNouvelleTransparence/SgNouvelleTransparence.tsx
+++ apps/webapp/src/secretariat-general/adapters/primary/components/SgNouvelleTransparence/SgNouvelleTransparence.tsx
@@ -3,45 +3,105 @@ import { ButtonsGroup } from "@codegouvfr/react-dsfr/ButtonsGroup";
 import { RadioButtons } from "@codegouvfr/react-dsfr/RadioButtons";
 import { formationToLabel } from "../../../../../reports/adapters/primary/labels/labels-mappers";
 import { Magistrat } from "shared-models";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-/* TODO AJOUTER DES VALIDATEURS ICI, ZOD OU REQUIRED PEUVENT SUFFIRE */
-// RECUPERER LES VALEURS DANS UN FORMULAIRE
+const schema = z.object({
+  transparenceName: z.string().min(1, "Le nom de la transparence est requise"),
+  dateEcheance: z
+    .string()
+    .min(1, "La date d'écheance de la transparenceest requise"),
+  formation: z.nativeEnum(Magistrat.Formation),
+});
+
+type Schema = z.infer<typeof schema>;
+
+const defaultValues: Schema = {
+  transparenceName: "",
+  dateEcheance: "",
+  formation: Magistrat.Formation.SIEGE,
+};
 
 export const SgNouvelleTransparence = () => {
-  const nativeInputProps = {
-    autoCorrect: "off",
-    autoCapitalize: "off",
-    autoComplete: "email",
-    spellCheck: false,
-    required: true,
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Schema>({
+    resolver: zodResolver(schema),
+    defaultValues,
+  });
+
+  const onSubmit: SubmitHandler<Schema> = (data) => {
+    console.log("Form submitted:", data);
   };
 
   return (
-    <form className="m-auto max-w-[480px]">
-      <Input
-        label="Nom de la transparence"
-        id="transparence-name"
-        nativeInputProps={{ ...nativeInputProps }}
+    <form className="m-auto max-w-[480px]" onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="transparenceName"
+        control={control}
+        render={({ field: { value, onChange, ...field } }) => (
+          <Input
+            label="Nom de la transparence"
+            id="transparence-name"
+            nativeInputProps={{
+              value: value || "",
+              onChange,
+              ...field,
+            }}
+            state={errors.transparenceName ? "error" : "default"}
+            stateRelatedMessage={errors.transparenceName?.message}
+          />
+        )}
       />
-      <Input label="Date d'échéance" id="date-echeance" {...nativeInputProps} />
-      <RadioButtons
-        legend="Formation"
+      <Controller
+        name="dateEcheance"
+        control={control}
+        render={({ field: { value, onChange, ...field } }) => (
+          <Input
+            label="Date d'échéance"
+            id="date-echeance"
+            nativeInputProps={{
+              type: "date",
+              value: value || "",
+              onChange,
+              ...field,
+            }}
+            state={errors.dateEcheance ? "error" : "default"}
+            stateRelatedMessage={errors.dateEcheance?.message}
+          />
+        )}
+      />
+      <Controller
         name="formation"
-        orientation="horizontal"
-        options={[
-          {
-            label: formationToLabel(Magistrat.Formation.SIEGE),
-            nativeInputProps: {
-              value: Magistrat.Formation.SIEGE,
-            },
-          },
-          {
-            label: formationToLabel(Magistrat.Formation.PARQUET),
-            nativeInputProps: {
-              value: Magistrat.Formation.PARQUET,
-            },
-          },
-        ]}
+        control={control}
+        render={({ field: { value, onChange } }) => (
+          <RadioButtons
+            legend="Formation"
+            orientation="horizontal"
+            options={[
+              {
+                label: formationToLabel(Magistrat.Formation.SIEGE),
+                nativeInputProps: {
+                  value: Magistrat.Formation.SIEGE,
+                  checked: value === Magistrat.Formation.SIEGE,
+                  onChange: () => onChange(Magistrat.Formation.SIEGE),
+                },
+              },
+              {
+                label: formationToLabel(Magistrat.Formation.PARQUET),
+                nativeInputProps: {
+                  value: Magistrat.Formation.PARQUET,
+                  checked: value === Magistrat.Formation.PARQUET,
+                  onChange: () => onChange(Magistrat.Formation.PARQUET),
+                },
+              },
+            ]}
+          />
+        )}
       />
       <ButtonsGroup
         buttons={[
@@ -49,6 +109,10 @@ export const SgNouvelleTransparence = () => {
             id: "annuler",
             children: "Annuler",
             priority: "tertiary",
+            type: "reset",
+            onClick: () => {
+              reset(defaultValues);
+            },
           },
           {
             id: "enregistrer",
