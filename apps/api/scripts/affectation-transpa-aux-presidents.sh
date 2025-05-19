a735a119e34ebea02bd9d897d958b2cba44614f1
Maxime Naulleau <maxime.naulleau@beta.gouv.fr>
2025-05-19 19:17:40 +0200

    Fix chemin du script

--- apps/api/scripts/affectation-transpa-aux-presidents.sh
+++ apps/api/scripts/affectation-transpa-aux-presidents.sh
@@ -4,4 +4,4 @@ scalingo \
     --region $SCALINGO_APP_REGION \
     --app $SCALINGO_APP_NAME \
     run \
-    "node ./apps/api/dist/cli/user-registration/affectation-transpa-aux-presidents.js"
+    "node ./apps/api/dist/cli/affectation-transpa-aux-presidents.js"
