48ed70aeaf8eb4a08d513f8a11cebbd7b26b37c6
Ebrard Alexis <dakie62@MacBook-Pro-de-Ebrard.local>
2025-06-29 21:00:39 +0200

    Deconnection de l'utilisateur et redirection sur la page d'accueil

--- apps/client/src/components/layout/AppHeaderAvatar.tsx
+++ apps/client/src/components/layout/AppHeaderAvatar.tsx
@@ -1,8 +1,37 @@
-import { colors } from "@codegouvfr/react-dsfr";
-import type { FC } from "react";
+import { colors } from '@codegouvfr/react-dsfr';
+import type { FC } from 'react';
+import { apiFetch } from '../../utils/api-fetch.utils';
+import type { IdentityAndAccessRestContract } from 'shared-models';
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+
+const logoutUser = async () => {
+  const {
+    method,
+    path
+  }: Partial<IdentityAndAccessRestContract['endpoints']['logout']> = {
+    method: 'POST',
+    path: 'logout'
+  };
+  await apiFetch(`/auth/${path}`, {
+    method
+  });
+};
 
 export const AppHeaderAvatar: FC = () => {
-  const firstLetters = "AB";
+  const firstLetters = 'AB';
+
+  const navigate = useNavigate();
+  const { mutate } = useMutation({
+    mutationFn: logoutUser,
+    onSuccess: () => {
+      navigate('/login');
+    }
+  });
+
+  const onClickLogout = async () => {
+    mutate();
+  };
 
   return (
     <div className="fr-btn flex items-center gap-2">
@@ -10,7 +39,7 @@ export const AppHeaderAvatar: FC = () => {
         id={`avatar-${firstLetters}`}
         className="flex h-8 w-8 items-center justify-center rounded-full text-sm font-medium text-white"
         style={{
-          backgroundColor: colors.decisions.text.title.blueFrance.default,
+          backgroundColor: colors.decisions.text.title.blueFrance.default
         }}
         title={firstLetters}
       >
@@ -18,7 +47,7 @@ export const AppHeaderAvatar: FC = () => {
       </div>
       <div
         id="avatar-logout"
-        // onClick={onClickLogout}
+        onClick={onClickLogout}
         className="font-semibold hover:cursor-pointer"
       >
         Se déconnecter
