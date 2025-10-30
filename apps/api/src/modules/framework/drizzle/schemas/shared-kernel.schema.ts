20ca0e8dce7125579c1cf80c85372f2d6411f92b
Jordan QUAGLIATINI <jordan.quagliatini@beta.gouv.fr>
2025-10-30 16:26:56 +0100

    fix: drizzle config with new files

--- apps/api/src/modules/framework/drizzle/schemas/shared-kernel.schema.ts
+++ apps/api/src/modules/framework/drizzle/schemas/shared-kernel.schema.ts
@@ -10,6 +10,21 @@ import {
 
 export const sharedKernelContextSchema = pgSchema('shared_kernel_context');
 
+export const transparencyEnum = pgEnum('transparency', [
+  'AUTOMNE_2024',
+  'PROCUREURS_GENERAUX_8_NOVEMBRE_2024',
+  'PROCUREURS_GENERAUX_25_NOVEMBRE_2024',
+  'TABLEAU_GENERAL_T_DU_25_NOVEMBRE_2024',
+  'CABINET_DU_MINISTRE_DU_21_JANVIER_2025',
+  'SIEGE_DU_06_FEVRIER_2025',
+  'PARQUET_DU_06_FEVRIER_2025',
+  'PARQUET_DU_20_FEVRIER_2025',
+  'DU_03_MARS_2025',
+  'GRANDE_TRANSPA_DU_21_MARS_2025',
+  'DU_30_AVRIL_2025',
+  'MARCH_2026',
+]);
+
 export const domainEventStatusEnum = sharedKernelContextSchema.enum(
   'domain_event_status',
   ['NEW', 'PENDING', 'CONSUMED'],
