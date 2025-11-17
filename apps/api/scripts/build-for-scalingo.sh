72d61ae7b6396f0b3e422fc854ccee565acce748
Jordan QUAGLIATINI <jordan.quagliatini@beta.gouv.fr>
2025-11-17 10:49:07 +0100

    chore: adds prisma files into release archive

--- apps/api/scripts/build-for-scalingo.sh
+++ apps/api/scripts/build-for-scalingo.sh
@@ -4,33 +4,33 @@
 mkdir .tmp-build/
 
 # Copy monorepo root files
-cp -v pnpm-lock.yaml package.json pnpm-workspace.yaml .tmp-build/
+cp -v \
+  package.json \
+  pnpm-lock.yaml \
+  pnpm-workspace.yaml \
+  .tmp-build/
 
 # Copy shared packages
 mkdir -v .tmp-build/packages/
-cp -rv packages/shared-models/ ./.tmp-build/packages/shared-models/
-
-# Create folder to copy api/ production files
-mkdir -pv .tmp-build/apps/api/dist
+cp -rv \
+  packages/shared-models/ \
+  .tmp-build/packages/shared-models/
 
 # Copy api files needed for production
-cp -v apps/api/package.json .tmp-build/apps/api/
-cp -v apps/api/drizzle.config.ts .tmp-build/apps/api/
-cp -rv apps/api/drizzle .tmp-build/apps/api/
-cp -rv apps/api/dist .tmp-build/apps/api/
-# Copy scalingo specific files
-cp -v apps/api/scalingo/.buildpacks apps/api/scalingo/Procfile .tmp-build/
+mkdir -pv .tmp-build/apps/api
+cp -rv \
+  apps/api/package.json \
+  apps/api/drizzle.config.ts \
+  apps/api/prisma.config.ts \
+  apps/api/drizzle \
+  apps/api/prisma \
+  apps/api/dist \
+  .tmp-build/apps/api/
 
-# The buildpack used to deploy api (see Procfile) runs "pnpm install", "pnpm build" and "pnpm prune --prod"
-# Our code is already built and we don't include the source so we skip the "build" in root package.json
-# /!\ WARNING /!\ these sed commands are NOT compatible with MacOS (add '' after -i if you want to run it on your laptop)
-sed -i 's/pnpm run -r build/echo 1/' .tmp-build/package.json
-# We don't need these
-sed -i /\"lint\":/d .tmp-build/package.json
-sed -i /\"lint:check\":/d .tmp-build/package.json
-sed -i /\"format\":/d .tmp-build/package.json
-sed -i /\"format:check\":/d .tmp-build/package.json
-sed -i /\"types:check\":/d .tmp-build/package.json
+cp -v \
+  apps/api/scalingo/.buildpacks \
+  apps/api/scalingo/Procfile \
+  .tmp-build/
 
 # Make archive to upload the packaged built api
 tar -czf api-scalingo.tar.gz .tmp-build
