57e540e0fa990d067e5c53bb3cd703e67ab01118
Jordan Quagliatini <jordan.quagliatini@beta.gouv.fr>
2025-11-28 12:35:52 +0100

    feat: improves the staging notice (#78)

--- apps/client/src/components/layout/PageLayout.tsx
+++ apps/client/src/components/layout/PageLayout.tsx
@@ -1,23 +1,19 @@
 import type { PropsWithChildren } from 'react';
+
 import { AppFooter } from './AppFooter';
 import { AppHeader } from './Header';
-import { Notice } from '../shared/Notice';
+import { StagingAppLayout } from '../shared/StagingAppLayout';
 
 export const PageLayout: React.FC<PropsWithChildren> = ({ children }) => {
-  const isStaging = !import.meta.env.DEV && import.meta.env.VITE_DEPLOY_ENV === 'staging';
-
-  const notice = isStaging && (
-    <Notice title="Environnement hors production." description="Les données affichées sont fictives." />
-  );
-
   return (
-    <div className="flex h-screen flex-col">
-      <AppHeader />
-      {notice}
-      <main className="flex flex-grow">
-        <div className="flex-grow">{children}</div>
-      </main>
-      <AppFooter />
-    </div>
+    <StagingAppLayout>
+      <div className={`flex h-screen flex-col`}>
+        <AppHeader />
+        <main className="flex flex-grow">
+          <div className="flex-grow">{children}</div>
+        </main>
+        <AppFooter />
+      </div>
+    </StagingAppLayout>
   );
 };
---
+++ apps/client/src/components/shared/StagingAppLayout.tsx
@@ -0,0 +1,25 @@
+import { colors } from '@codegouvfr/react-dsfr';
+
+const textColor = colors.decisions.text.default.warning.default;
+const bgColor = colors.decisions.background.contrast.warning.default;
+
+export const StagingAppLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const isStaging = !import.meta.env.DEV && import.meta.env.VITE_DEPLOY_ENV === 'staging';
+
+  if (!isStaging) return children;
+
+  return (
+    <>
+      <div
+        className="fixed left-0 right-0 top-0 z-[1000] flex h-10 items-center justify-center"
+        style={{ color: textColor, backgroundColor: bgColor }}
+      >
+        <p className="mb-0 text-sm">
+          <span className="font-bold">Environnement hors-production.</span>
+          <span className="ml-1">Les données affichées sont fictives</span>
+        </p>
+      </div>
+      <div className="mt-10">{children}</div>
+    </>
+  );
+};
