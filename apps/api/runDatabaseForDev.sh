39e4d16675b7dd6a6ee1df519bd9425e84f094a6
Jordan QUAGLIATINI <jordan.quagliatini@beta.gouv.fr>
2025-12-30 18:26:34 +0100

    refactor(FON-000): docker compose and local environment

--- apps/api/runDatabaseForDev.sh
+++
@@ -1,3 +0,0 @@
-docker compose -f ./docker-compose-dev.yaml down --volumes
-docker compose -f ./docker-compose-dev.yaml down --remove-orphans
-docker compose -f ./docker-compose-dev.yaml --env-file ./.env.docker up
