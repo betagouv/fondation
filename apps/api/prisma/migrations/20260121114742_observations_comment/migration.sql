b507cd7d5b8d2de0305c0c056feb6cbe8b3e3b09
Ebrard Alexis <dakie62@MacBook-Pro-de-Ebrard.local>
2026-01-26 09:51:39 +0100

    chore: correction des migrations

--- apps/api/prisma/migrations/20260121114742_observations_comment/migration.sql
+++ apps/api/prisma/migrations/20260121114742_observations_comment/migration.sql
@@ -1,5 +1,17 @@
+/*
+  Warnings:
+
+  - Made the column `magistrat_id` on table `observation` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "nominations_context"."observation" DROP CONSTRAINT "observation_magistrat_id_fkey";
+
 -- AlterTable
-ALTER TABLE "nominations_context"."dossier_de_nomination" ALTER COLUMN "content" SET DEFAULT '{}'::JSONB;
+ALTER TABLE "nominations_context"."observation" ALTER COLUMN "magistrat_id" SET NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "nominations_context"."observation" ADD CONSTRAINT "observation_magistrat_id_fkey" FOREIGN KEY ("magistrat_id") REFERENCES "nominations_context"."magistrat"("id") ON DELETE RESTRICT ON UPDATE NO ACTION;
 
 -- CreateTable
 CREATE TABLE "nominations_context"."observation_member_comment" (
@@ -13,12 +25,12 @@ CREATE TABLE "nominations_context"."observation_member_comment" (
 );
 
 -- CreateTable
-CREATE TABLE "nominations_context"."observation_member_comment_file" (
+CREATE TABLE "nominations_context"."observation_member_comment_screenshot" (
     "user_id" UUID NOT NULL,
     "observation_id" UUID NOT NULL,
     "file_id" UUID NOT NULL,
 
-    CONSTRAINT "observation_member_comment_file_pkey" PRIMARY KEY ("user_id","observation_id","file_id")
+    CONSTRAINT "observation_member_comment_screenshot_pkey" PRIMARY KEY ("user_id","observation_id","file_id")
 );
 
 -- AddForeignKey
@@ -28,7 +40,7 @@ ALTER TABLE "nominations_context"."observation_member_comment" ADD CONSTRAINT "o
 ALTER TABLE "nominations_context"."observation_member_comment" ADD CONSTRAINT "observation_member_comment_observation_id_fkey" FOREIGN KEY ("observation_id") REFERENCES "nominations_context"."observation"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "nominations_context"."observation_member_comment_file" ADD CONSTRAINT "observation_member_comment_file_user_id_observation_id_fkey" FOREIGN KEY ("user_id", "observation_id") REFERENCES "nominations_context"."observation_member_comment"("user_id", "observation_id") ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE "nominations_context"."observation_member_comment_screenshot" ADD CONSTRAINT "observation_member_comment_screenshot_user_id_observation_id_fkey" FOREIGN KEY ("user_id", "observation_id") REFERENCES "nominations_context"."observation_member_comment"("user_id", "observation_id") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE "nominations_context"."observation_member_comment_file" ADD CONSTRAINT "observation_member_comment_file_file_id_fkey" FOREIGN KEY ("file_id") REFERENCES "files_context"."files"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE "nominations_context"."observation_member_comment_screenshot" ADD CONSTRAINT "observation_member_comment_screenshot_file_id_fkey" FOREIGN KEY ("file_id") REFERENCES "files_context"."files"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
