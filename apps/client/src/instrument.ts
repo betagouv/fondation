ea14533444e30e8cbb34e23f0d24f397771cb275
Jordan Quagliatini <jordan.quagliatini@beta.gouv.fr>
2026-03-09 20:18:35 +0100

    chore(FON-000): sentry release (#269)

--- apps/client/src/instrument.ts
+++ apps/client/src/instrument.ts
@@ -1,10 +1,11 @@
 import * as Sentry from '@sentry/react';
 import { useEffect } from 'react';
-import { useLocation, useNavigationType, createRoutesFromChildren, matchRoutes } from 'react-router';
+import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-router';
 
 Sentry.init({
   environment: import.meta.env.VITE_DEPLOY_ENV,
   dsn: import.meta.env.VITE_SENTRY_DSN,
+  release: [`fondation-client`, import.meta.env.VITE_TAGGED_VERSION].filter((x) => !!x?.trim()).join('@'),
   integrations: [
     Sentry.reactRouterV7BrowserTracingIntegration({
       useEffect,
