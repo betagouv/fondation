06a202ea740f130b92b9c57d9cc6e5d6d9e775ac
Jordan Quagliatini <jordan.quagliatini@beta.gouv.fr>
2026-04-29 17:02:38 +0200

    chore(FON-000): removes fast-xml-parser (#323)

--- apps/api/src/modules/maintenance/infrastructure/maintenance.service.ts
+++ apps/api/src/modules/maintenance/infrastructure/maintenance.service.ts
@@ -1,19 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { IngestXmlJurisdiction } from './ingest-xml-jurisdiction.use-case';
-import { IngestXmlMagistrat } from './ingest-xml-magistrat.use-case';
 
 @Injectable()
-export class MaintenanceService {
-  constructor(
-    private readonly ingestXmlJurisdiction: IngestXmlJurisdiction,
-    private readonly ingestXmlMagistrat: IngestXmlMagistrat,
-  ) {}
-
-  ingestXmlJurisdictions(buffer: Buffer): Promise<{ updated: number }> {
-    return this.ingestXmlJurisdiction.execute(buffer);
-  }
-
-  ingestXmlMagistrats(buffer: Buffer): Promise<{ updated: number }> {
-    return this.ingestXmlMagistrat.execute(buffer);
-  }
-}
+export class MaintenanceService {}
