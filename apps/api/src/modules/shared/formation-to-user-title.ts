5304666cce32ad7ec4b979c916dc659758932f81
Jordan Quagliatini <jordan.quagliatini@beta.gouv.fr>
2026-04-30 15:53:59 +0200

    fix(FON-204): retrieve chairmen of formation (#324)

---
+++ apps/api/src/modules/shared/formation-to-user-title.ts
@@ -0,0 +1,15 @@
+import { Magistrat } from 'shared-models';
+import { USER_TITLES, UserTitleEnum } from '../administration/domain/user-enum';
+
+export function formationToUserTitle(
+  formation: Magistrat.Formation | null | undefined,
+): (UserTitleEnum | undefined | null)[] {
+  switch (formation) {
+    case Magistrat.Formation.PARQUET:
+      return ['DEPUTY_PRESIDENT_PARQUET', 'PRESIDENT_PARQUET'];
+    case Magistrat.Formation.SIEGE:
+      return ['DEPUTY_PRESIDENT_SIEGE', 'PRESIDENT_SIEGE'];
+    default:
+      return [...USER_TITLES];
+  }
+}
