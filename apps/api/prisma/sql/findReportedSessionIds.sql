48b6f05391ec82e685861fb9e8d26445362282df
Jordan QUAGLIATINI <jordan.quagliatini@beta.gouv.fr>
2026-06-04 11:29:15 +0200

    fix(FON-128): archivable uses official report outcome

--- apps/api/prisma/sql/findReportedSessionIds.sql
+++ apps/api/prisma/sql/findReportedSessionIds.sql
@@ -10,16 +10,19 @@ WHERE
 
   AND NOT EXISTS (
     SELECT 1
-    FROM nominations_context.dossier_de_nomination 
-    WHERE session_id = s.id AND (
-      outcome IS NULL
-      OR outcome != ALL('{VALIDATED,NON_VALIDATED,WITHDRAWN,REMOVED}'::nominations_context.nomination_file_outcome_enum[])
-    )
-  )
-
-  AND NOT EXISTS (
-    SELECT 1
     FROM nominations_context.dossier_de_nomination ddn
-      LEFT JOIN docs.official_report_nomination_file ornf ON ornf.nomination_file_id = ddn.id
-    WHERE ddn.session_id = s.id AND ornf.nomination_file_id IS NULL
+    WHERE
+      ddn.session_id = s.id
+      AND (
+        outcome IS NULL
+        OR outcome != ALL('{VALIDATED,NON_VALIDATED,WITHDRAWN,REMOVED}'::nominations_context.nomination_file_outcome_enum[])
+        OR NOT EXISTS (
+          SELECT 1
+          FROM docs.official_report_nomination_file ornf
+          WHERE (
+            ornf.nomination_file_id = ddn.id
+            AND ornf.outcome = ANY('{VALIDATED,NON_VALIDATED,WITHDRAWN}'::docs.agenda_file_outcome_enum[])
+          )
+        )
+      )
   );
