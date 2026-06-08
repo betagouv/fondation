4ec3a6d3aa89a2fcfb0bbfdc86dbcf955864c9ea
Jordan QUAGLIATINI <jordan.quagliatini@beta.gouv.fr>
2026-06-08 16:35:47 +0200

    fix(FON-448): agenda files selection buttons design

--- apps/client/src/components/shared/nomination-files-table/components/AgendaBasketButtons.tsx
+++ apps/client/src/components/shared/nomination-files-table/components/AgendaBasketButtons.tsx
@@ -1,17 +1,15 @@
 import Button from '@codegouvfr/react-dsfr/Button';
-import { cx } from '@codegouvfr/react-dsfr/fr/cx';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { generatePath, useNavigate } from 'react-router';
 
 import { useSelectedFileIds } from '../contexts/files-selection.context';
 import { useNominationFilesTable } from '../contexts/files-table.context';
-import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from '@/components/shared/menu';
 import { useAgendaBasket } from '@/pages/secretariat-general/docs/agenda/hooks/useAgendaBasket.hook';
 import { ROUTE_PATHS } from '@/utils/route-path.utils';
 import { useIsSessionReadyForDocGenerationQuery } from '@queries/agenda.queries';
 
-export function AgendaBasketButton() {
+export function AgendaBasketButtons() {
   const navigate = useNavigate();
   const { sessionId } = useNominationFilesTable();
   const selectedIds = useSelectedFileIds();
@@ -29,41 +27,27 @@ export function AgendaBasketButton() {
 
   if (!readiness?.canCreateAgenda) return null;
 
-  if (basket.isEmpty) {
-    return (
+  return (
+    <>
       <Button
         size="small"
         priority="secondary"
         iconId="fr-icon-file-add-line"
         onClick={onAddToBasket}
+        className="grow"
         disabled={selectedIds.length === 0}
       >
         <FormattedMessage defaultMessage={"Ajouter à l'ODJ"} />
       </Button>
-    );
-  }
-
-  return (
-    <MenuRoot>
-      <MenuTrigger size="small" priority="secondary" iconId="fr-icon-folder-2-line">
-        ODJ ({basket.size}) <i className={cx('ri-arrow-down-s-line')} />
-      </MenuTrigger>
-
-      <MenuContent>
-        <MenuItem onClick={onAddToBasket} disabled={selectedIds.length === 0} iconId="fr-icon-file-add-line">
-          <FormattedMessage
-            values={{ count: basket.size }}
-            defaultMessage={`{count, plural,
-              =0 {Ajouter à l'ODJ}
-              other {Ajouter à l'ODJ ({count})}
-            }`}
-          />
-        </MenuItem>
-
-        <MenuItem onClick={onStartAgenda}>
-          <FormattedMessage defaultMessage={"Générer l'ODJ"} />
-        </MenuItem>
-      </MenuContent>
-    </MenuRoot>
+      <Button size="small" onClick={onStartAgenda} disabled={basket.isEmpty} className="grow">
+        <FormattedMessage
+          values={{ count: basket.size }}
+          defaultMessage={`{count, plural,
+            =0 {Générer l'ODJ}
+            other {Générer l'ODJ ({count, number})}
+          }`}
+        />
+      </Button>
+    </>
   );
 }
