cc008f2c7114bd2fe355de1bab00a9dcd44860bc
Jordan Quagliatini <jordan.quagliatini@beta.gouv.fr>
2026-06-09 15:36:06 +0200

    fix(FON-444): agenda filter out already reported files (#397)

--- apps/api/src/modules/docs/docs.e2e-spec.ts
+++ apps/api/src/modules/docs/docs.e2e-spec.ts
@@ -207,6 +207,6 @@ describe('Docs Service', () => {
         sessionMeetingDate: { day: 11, month: 2, year: 2026 },
         nominationFileIds: [nominationFiles.items[0]!.id],
       } satisfies CreateOrUpdateAgendaDto)
-      .expect(HttpStatus.CONFLICT);
+      .expect(HttpStatus.BAD_REQUEST);
   });
 });
