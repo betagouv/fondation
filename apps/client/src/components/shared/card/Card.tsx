a4fc2d2e278f21ab2cd0912941a4f3b3f3982649
Jordan QUAGLIATINI <jordan.quagliatini@beta.gouv.fr>
2026-06-12 11:50:39 +0200

    chore(FON-000): shared components structure proposal

--- apps/client/src/components/shared/card/Card.tsx
+++ apps/client/src/components/shared/card/Card.tsx
@@ -1,5 +1,6 @@
 import { Card as DsfrCard } from '@codegouvfr/react-dsfr/Card';
 import type { RegisteredLinkProps } from '@codegouvfr/react-dsfr/link';
+import clsx from 'clsx';
 
 export type CardProps = {
   title: string;
@@ -7,16 +8,14 @@ export type CardProps = {
   linkProps: RegisteredLinkProps;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-export const Card = ({ title, description, className, linkProps }: CardProps) => {
+export function Card({ title, description, className, linkProps }: CardProps) {
   return (
-    <div className="container">
-      <DsfrCard
-        className={`${className}`}
-        title={title}
-        desc={description}
-        enlargeLink
-        linkProps={linkProps}
-      />
-    </div>
+    <DsfrCard
+      className={clsx('container', className)}
+      title={title}
+      desc={description}
+      enlargeLink
+      linkProps={linkProps}
+    />
   );
-};
+}
