01373968c492371cb41f00b43701546b12de7256
Jessica Kossibale <189719444+jessicakossibale@users.noreply.github.com>
2026-08-27 18:41:08 +0200

    fix(FON-535): ship the document assets package to scalingo

--- apps/api/scripts/build-for-scalingo.sh
+++ apps/api/scripts/build-for-scalingo.sh
@@ -15,14 +15,17 @@ pnpm --filter api... install --frozen-lockfile --ignore-scripts && \
 
 temp=$(basename $(mktemp -d)) && \
   mkdir -p "$temp" && \
-  find apps/api/dist \
+  find apps/api/dist packages/documents-assets/dist \
     -iname '*.d.ts' -type f -delete \
     -or -iname '*.map' -type f -delete \
     -or -iname '*.tsbuildinfo' -type f -delete && \
   mv bin pnpm-lock.yaml pnpm-workspace.yaml vendor "$temp" && \
-  mkdir -p "$temp/apps/api" && \
+  mkdir -p "$temp/apps/api" "$temp/packages/documents-assets" && \
   mv apps/api/dist "$temp/apps/api" && \
+  mv packages/documents-assets/{dist,assets} "$temp/packages/documents-assets" && \
   jq 'del(.scripts)' package.json > "$temp/package.json" && \
   jq 'del(.scripts)' apps/api/package.json > "$temp/apps/api/package.json" && \
+  jq 'del(.scripts)' packages/documents-assets/package.json \
+    > "$temp/packages/documents-assets/package.json" && \
   mv apps/api/scalingo/{.buildpacks,Procfile} "$temp" && \
   tar -czf api-scalingo.tar.gz "$temp"
